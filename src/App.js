98d1d81cbf6618f77dfc0eb617ff25e10d69cc01
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-05-15 18:34:13 -0300

    material-ui installed and theme started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-	return <div>Hello</div>;
-}
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const App = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<div>Hello</div>
+		</ThemeProvider>
+	);
+};
 
 export default App;
+
+const theme = createMuiTheme({});
